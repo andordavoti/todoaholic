31dac9505a8a1c907d5eba7eae707624203cf316
andordavoti <andor.davoti@gmail.com>
2021-12-23 23:31:43 +0100

    added email and password auth

---
+++ lib/components/authgate.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutterfire_ui/auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        // User is not signed in
+        if (!snapshot.hasData) {
+          return const SignInScreen(providerConfigs: [
+            EmailProviderConfiguration(),
+          ]);
+        }
+
+        // Render your application if authenticated
+        return Container(color: Colors.blue);
+      },
+    );
+  }
+}
