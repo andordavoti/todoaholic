737fee53962d6be31e464fa9edb93565296c9826
andordavoti <andor.davoti@gmail.com>
2021-12-26 17:26:59 +0100

    built out a simple version of the timeline screen

--- lib/screens/timeline_screen.dart
+++ lib/screens/timeline_screen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:grouped_list/grouped_list.dart';
+import 'package:provider/provider.dart';
+import 'package:todoaholic/components/todo_item.dart';
+import 'package:todoaholic/data/todo.dart';
+import 'package:todoaholic/data/todo_dao.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 
 class TimelineScreen extends StatelessWidget {
   const TimelineScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final todoDao = Provider.of<TodoDao>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         actions: [
@@ -17,9 +25,62 @@ class TimelineScreen extends StatelessWidget {
         title: const Align(
             alignment: Alignment.topCenter, child: Text('Timeline')),
       ),
-      body: Container(
-        padding: const EdgeInsets.all(16),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: todoDao.getTodoTimelineStream(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text('Something went wrong...'),
+            );
+          }
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.data!.docs.isEmpty) {
+            return Center(
+              child: Flex(
+                direction: Axis.vertical,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Nothing to do...',
+                    style: Theme.of(context).textTheme.headline2,
+                    textAlign: TextAlign.center,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8),
+                    child: Text(
+                      'Go back and add some tasks',
+                      style: Theme.of(context).textTheme.bodyText2,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }
+          return _buildList(context, snapshot.data!.docs);
+        },
       ),
     );
+  }
+
+  Widget _buildList(BuildContext context, List<DocumentSnapshot>? snapshot) {
+    return GroupedListView<Todo, String>(
+      elements: snapshot!.map((doc) => Todo.fromSnapshot(doc)).toList(),
+      groupBy: (element) => element.date.toString(),
+      groupSeparatorBuilder: (groupByValue) => Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Text(DateFormat.yMMMd().format(DateTime.parse(groupByValue)),
+            style: Theme.of(context).textTheme.headline2),
+      ),
+      itemBuilder: (context, element) => _buildListItem(context, element),
+    );
+  }
+
+  Widget _buildListItem(BuildContext context, Todo element) {
+    return TodoItem(element);
   }
 }
