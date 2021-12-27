623e2317558fe0c995841a8119cf141b9cbd9068
andordavoti <andor.davoti@gmail.com>
2021-12-27 16:26:01 +0100

    set the right color statusbar depending on device theme

--- lib/components/authgate.dart
+++ lib/components/authgate.dart
@@ -2,12 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:flutterfire_ui/auth.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:todoaholic/screens/home.dart';
+import 'package:flutter/services.dart';
 
 class AuthGate extends StatelessWidget {
   const AuthGate({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final isDarkMode =
+        MediaQuery.of(context).platformBrightness == Brightness.dark;
+
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: isDarkMode ? Colors.white : Colors.black,
+      statusBarBrightness: isDarkMode ? Brightness.dark : Brightness.light,
+    ));
     return StreamBuilder<User?>(
       stream: FirebaseAuth.instance.authStateChanges(),
       builder: (context, snapshot) {
