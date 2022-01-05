9d19016e81f0cd9910b4bbdab8d12203be9c8a9e
andordavoti <andor.davoti@gmail.com>
2022-01-05 20:05:47 +0100

    press "+" on the keyboard to add a task

--- client/lib/screens/home.dart
+++ client/lib/screens/home.dart
@@ -26,120 +26,137 @@ class Home extends StatelessWidget {
     final todoDao = Provider.of<TodoDao>(context, listen: false);
 
     return Consumer<AppState>(builder: (context, appState, child) {
-      return Scaffold(
-        body: StreamBuilder<QuerySnapshot>(
-            stream: todoDao.getUndonePastStream(),
-            builder: (context, pastSnapshot) {
-              final bool pastTasksExist =
-                  pastSnapshot.hasData && pastSnapshot.data!.docs.isNotEmpty;
-
-              return appState.selectedDate == currentDate && pastTasksExist
-                  ? SingleChildScrollView(
-                      physics: const BouncingScrollPhysics(),
-                      controller: _scrollController,
-                      child: Column(
-                        children: [
-                          const PastTodoList(type: TodoItemType.past),
-                          TodoList(noPastTasks: false),
-                        ],
-                      ),
-                    )
-                  : TodoList(
-                      noPastTasks: true,
-                    );
-            }),
-        resizeToAvoidBottomInset: false,
-        floatingActionButton: Padding(
-          padding: const EdgeInsets.only(left: 20, right: 20),
-          child: Stack(
-            children: <Widget>[
-              Align(
-                alignment: Alignment.bottomLeft,
-                child: DateNavigation(
-                  leftAction: () {
-                    appState.decrementSelectedDate();
-                  },
-                  leftLongPressAction: () {
-                    appState.setSelectedDate(DateTime.now());
-                  },
-                  rightAction: () {
-                    appState.incrementSelectedDate();
-                  },
-                  rightLongPressAction: () {
-                    appState.setSelectedDate(
-                        appState.selectedDate.add(const Duration(days: 7)));
-                  },
-                ),
+      return RawKeyboardListener(
+        autofocus: true,
+        focusNode: FocusNode(
+            onKey: (FocusNode node, RawKeyEvent event) =>
+                KeyEventResult.handled),
+        onKey: (event) {
+          if (event.isKeyPressed(LogicalKeyboardKey.add)) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const ManageTodoScreen(null),
               ),
-              Align(
-                alignment: Alignment.bottomRight,
-                child: FloatingActionButton(
-                  onPressed: () {
-                    HapticFeedback.selectionClick();
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const ManageTodoScreen(null)),
-                    );
-                  },
-                  child: const Icon(Icons.add),
+            );
+          }
+        },
+        child: Scaffold(
+          body: StreamBuilder<QuerySnapshot>(
+              stream: todoDao.getUndonePastStream(),
+              builder: (context, pastSnapshot) {
+                final bool pastTasksExist =
+                    pastSnapshot.hasData && pastSnapshot.data!.docs.isNotEmpty;
+
+                return appState.selectedDate == currentDate && pastTasksExist
+                    ? SingleChildScrollView(
+                        physics: const BouncingScrollPhysics(),
+                        controller: _scrollController,
+                        child: Column(
+                          children: [
+                            const PastTodoList(type: TodoItemType.past),
+                            TodoList(noPastTasks: false),
+                          ],
+                        ),
+                      )
+                    : TodoList(
+                        noPastTasks: true,
+                      );
+              }),
+          resizeToAvoidBottomInset: false,
+          floatingActionButton: Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: Stack(
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.bottomLeft,
+                  child: DateNavigation(
+                    leftAction: () {
+                      appState.decrementSelectedDate();
+                    },
+                    leftLongPressAction: () {
+                      appState.setSelectedDate(DateTime.now());
+                    },
+                    rightAction: () {
+                      appState.incrementSelectedDate();
+                    },
+                    rightLongPressAction: () {
+                      appState.setSelectedDate(
+                          appState.selectedDate.add(const Duration(days: 7)));
+                    },
+                  ),
                 ),
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: FloatingActionButton(
+                    onPressed: () {
+                      HapticFeedback.selectionClick();
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const ManageTodoScreen(null)),
+                      );
+                    },
+                    child: const Icon(Icons.add),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerFloat,
+          appBar: AppBar(
+            title: Align(
+              alignment: Alignment.topCenter,
+              child: InkWell(
+                  onTap: () async {
+                    final currentDate = DateTime.now();
+
+                    final pickedDate = await showDatePicker(
+                      context: context,
+                      initialDate: appState.selectedDate,
+                      firstDate: DateTime(currentDate.year - 5),
+                      lastDate: DateTime(currentDate.year + 5),
+                    );
+
+                    if (pickedDate != null) {
+                      appState.setSelectedDate(pickedDate);
+                    }
+                  },
+                  child: Text(DateFormat('EEEE, MMMM d')
+                      .format(appState.selectedDate))),
+            ),
+            leading: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: IconButton(
+                    icon: Icon(Icons.person_sharp,
+                        color: Theme.of(context).textTheme.bodyText2!.color),
+                    onPressed: () {
+                      HapticFeedback.selectionClick();
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const UserProfileScreen()),
+                      );
+                    })),
+            actions: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: (IconButton(
+                    icon: Icon(Icons.timeline,
+                        color: Theme.of(context).textTheme.bodyText2!.color),
+                    onPressed: () {
+                      HapticFeedback.selectionClick();
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const TimelineScreen(),
+                        ),
+                      );
+                    })),
               ),
             ],
           ),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        appBar: AppBar(
-          title: Align(
-            alignment: Alignment.topCenter,
-            child: InkWell(
-                onTap: () async {
-                  final currentDate = DateTime.now();
-
-                  final pickedDate = await showDatePicker(
-                    context: context,
-                    initialDate: appState.selectedDate,
-                    firstDate: DateTime(currentDate.year - 5),
-                    lastDate: DateTime(currentDate.year + 5),
-                  );
-
-                  if (pickedDate != null) {
-                    appState.setSelectedDate(pickedDate);
-                  }
-                },
-                child: Text(
-                    DateFormat('EEEE, MMMM d').format(appState.selectedDate))),
-          ),
-          leading: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: IconButton(
-                  icon: Icon(Icons.person_sharp,
-                      color: Theme.of(context).textTheme.bodyText2!.color),
-                  onPressed: () {
-                    HapticFeedback.selectionClick();
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const UserProfileScreen()),
-                    );
-                  })),
-          actions: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: (IconButton(
-                  icon: Icon(Icons.timeline,
-                      color: Theme.of(context).textTheme.bodyText2!.color),
-                  onPressed: () {
-                    HapticFeedback.selectionClick();
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const TimelineScreen(),
-                      ),
-                    );
-                  })),
-            ),
-          ],
         ),
       );
     });
