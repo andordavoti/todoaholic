fb3fc0bd91d339f5efb69ed3c25d285606ef6b2c
andordavoti <andor.davoti@gmail.com>
2022-01-08 15:33:58 +0100

    add the google-services.json to the encrypted android keys folder

--- .github/scripts/decrypt_android_secrets.sh
+++ .github/scripts/decrypt_android_secrets.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-gpg --output ../../client/android/android_keys.zip --batch --yes --decrypt --passphrase="$ANDROID_KEYS_SECRET_PASSPHRASE" ../../client/android/android_keys.zip.gpg && jar xvf ../../client/android/android_keys.zip
+gpg --output ../../client/android/android_keys.zip --batch --yes --decrypt --passphrase="$ANDROID_KEYS_SECRET_PASSPHRASE" ../../client/android/android_keys.zip.gpg && jar xvf ../../client/android/android_keys.zip && mv ../../client/android/google-services.json ../../client/android/app
