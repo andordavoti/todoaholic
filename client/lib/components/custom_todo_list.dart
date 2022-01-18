217a11b38da03d679ccc9c63cd15ae22916a1d93
andordavoti <andor.davoti@gmail.com>
2022-01-18 20:27:19 +0100

    custom list screen

---
+++ client/lib/components/custom_todo_list.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:todoaholic/components/todo_item.dart';
+import 'package:todoaholic/data/app_state_provider.dart';
+import 'package:todoaholic/data/custom_list_dao.dart';
+import 'package:todoaholic/data/todo.dart';
+import 'package:todoaholic/data/todo_dao.dart';
+import 'package:provider/provider.dart';
+import 'package:todoaholic/data/todo_item_type.dart';
+
+class CustomTodoList extends StatelessWidget {
+  final ScrollController _scrollController = ScrollController();
+
+  CustomTodoList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final customListDao = Provider.of<CustomListDao>(context, listen: false);
+
+    return Consumer<AppState>(builder: (context, appState, child) {
+      if (appState.selectedList != null) {
+        return StreamBuilder<QuerySnapshot>(
+          stream: customListDao.getStream(appState.selectedList!),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return const Center(
+                child: Text('Something went wrong...'),
+              );
+            }
+            if (!snapshot.hasData) {
+              return const Center(child: CircularProgressIndicator());
+            }
+
+            if (snapshot.data!.docs.isEmpty) {
+              return Center(
+                child: Flex(
+                  direction: Axis.vertical,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Nothing in this list',
+                      style: Theme.of(context).textTheme.headline2,
+                      textAlign: TextAlign.center,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8),
+                      child: Text(
+                        'Add something by tapping the + button',
+                        style: Theme.of(context).textTheme.bodyText1,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            }
+            return _buildList(context, snapshot.data!.docs);
+          },
+        );
+      } else {
+        return const Center(child: CircularProgressIndicator());
+      }
+    });
+  }
+
+  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
+    final todoDao = Provider.of<TodoDao>(context, listen: false);
+    return ReorderableListView(
+      onReorder: (int oldIndex, int newIndex) {
+        if (oldIndex < newIndex) {
+          newIndex -= 1;
+        }
+
+        final todoMoved = Todo.fromSnapshot(snapshot.elementAt(oldIndex));
+        todoDao.setOrder(todoMoved, newIndex);
+      },
+      shrinkWrap: true,
+      scrollController: _scrollController,
+      physics: const BouncingScrollPhysics(),
+      padding: EdgeInsets.only(
+          bottom: 56 +
+              kFloatingActionButtonMargin * 2 +
+              MediaQuery.of(context).padding.bottom),
+      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+    );
+  }
+
+  Widget _buildListItem(BuildContext context, DocumentSnapshot snapshot) {
+    final todo = Todo.fromSnapshot(snapshot);
+    return TodoItem(
+      todo,
+      TodoItemType.custom,
+      key: ObjectKey(todo),
+    );
+  }
+}
---
+++ client/lib/screens/custom_list_screen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:todoaholic/components/app_drawer.dart';
+import 'package:todoaholic/components/custom_todo_list.dart';
+import 'package:todoaholic/data/app_state_provider.dart';
+
+import 'manage_todo_screen.dart';
+
+class AddTaskIntent extends Intent {}
+
+class BackIntent extends Intent {}
+
+class CustomListScreen extends StatelessWidget {
+  static const String routeName = '/customList';
+
+  const CustomListScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<AppState>(builder: (context, appState, child) {
+      return Shortcuts(
+        shortcuts: {
+          LogicalKeySet(LogicalKeyboardKey.keyA): AddTaskIntent(),
+          LogicalKeySet(LogicalKeyboardKey.add): AddTaskIntent(),
+          LogicalKeySet(LogicalKeyboardKey.escape): BackIntent(),
+        },
+        child: Actions(
+          actions: {
+            AddTaskIntent: CallbackAction<AddTaskIntent>(
+                onInvoke: (intent) => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const ManageTodoScreen(null)),
+                    )),
+            BackIntent: CallbackAction<BackIntent>(
+                onInvoke: (intent) => Navigator.pop(context)),
+          },
+          child: Focus(
+            autofocus: true,
+            child: Scaffold(
+                drawer: const AppDrawer(),
+                appBar: AppBar(
+                  actions: const [
+                    IconButton(
+                      color: Colors.transparent,
+                      icon: SizedBox.shrink(),
+                      onPressed: null,
+                    )
+                  ],
+                  title: Align(
+                      alignment: Alignment.topCenter,
+                      child: Text(appState.selectedList?.name ?? "")),
+                ),
+                floatingActionButton: FloatingActionButton(
+                  onPressed: () {
+                    if (appState.selectedList != null) {
+                      HapticFeedback.selectionClick();
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const ManageTodoScreen(null)),
+                      );
+                    }
+                  },
+                  child: const Icon(Icons.add),
+                ),
+                body: CustomTodoList()),
+          ),
+        ),
+      );
+    });
+  }
+}
