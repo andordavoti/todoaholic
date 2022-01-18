6a41e7a6047133c0dcdd6944319da25239b52cdf
andordavoti <andor.davoti@gmail.com>
2022-01-18 20:24:04 +0100

    add selectedList to app state

--- client/lib/data/app_state_provider.dart
+++ client/lib/data/app_state_provider.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:todoaholic/data/custom_list.dart';
 import '../utils/datetime_extension.dart';
 
 class AppState extends ChangeNotifier {
   DateTime selectedDate = DateTime.now().getDateOnly();
+  CustomList? selectedList;
 
   void decrementSelectedDate() {
     selectedDate = selectedDate.add(const Duration(days: -1));
@@ -16,6 +18,11 @@ class AppState extends ChangeNotifier {
 
   void setSelectedDate(DateTime date) {
     selectedDate = date.getDateOnly();
+    notifyListeners();
+  }
+
+  void setSelectedList(CustomList list) {
+    selectedList = list;
     notifyListeners();
   }
 }
