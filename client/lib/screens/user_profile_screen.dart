ecb416d44b771dbf9808ed654aafa0d7ec921d5f
andordavoti <andor.davoti@gmail.com>
2022-01-22 18:06:37 +0100

    delete user account and verify email dialog

--- client/lib/screens/user_profile_screen.dart
+++ client/lib/screens/user_profile_screen.dart
@@ -1,9 +1,12 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:todoaholic/components/app_drawer.dart';
+import 'package:todoaholic/components/delete_account_dialog.dart';
+import 'package:todoaholic/components/verify_email_dialog.dart';
 import 'package:todoaholic/main.dart';
 import 'package:todoaholic/screens/routes.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -85,121 +88,206 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
                   ),
                 ],
               ),
-              body: ListView(children: [
-                Padding(
-                  padding: const EdgeInsets.all(32.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Center(
-                        child: UserAvatar(
-                          size: 81,
-                        ),
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.only(top: 16.0),
-                        child: EditableUserDisplayName(),
-                      ),
+              body: StreamBuilder<User?>(
+                  stream: FirebaseAuth.instance.userChanges(),
+                  builder: (context, snapshot) {
+                    final user = snapshot.data;
+                    return ListView(children: [
                       Padding(
-                        padding: const EdgeInsets.only(top: 32),
-                        child: Row(
+                        padding: const EdgeInsets.all(32.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            LinkedProvidersRow(
-                                providerConfigs: MyApp.providerConfigs),
+                            const Center(
+                              child: UserAvatar(
+                                size: 81,
+                              ),
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.only(top: 16.0),
+                              child: EditableUserDisplayName(),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 32),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  LinkedProvidersRow(
+                                      providerConfigs: MyApp.providerConfigs),
+                                ],
+                              ),
+                            ),
+                            user != null && user.emailVerified == false
+                                ? Padding(
+                                    padding: const EdgeInsets.only(
+                                        top: 32, bottom: 8),
+                                    child: SizedBox(
+                                      width: 275,
+                                      child: OutlinedButton.icon(
+                                          onPressed: () async {
+                                            HapticFeedback.selectionClick();
+                                            await user.sendEmailVerification();
+
+                                            showDialog(
+                                                context: context,
+                                                builder:
+                                                    (BuildContext context) {
+                                                  return VerifyEmailDialog(
+                                                    okAction: () async {
+                                                      user.reload();
+                                                    },
+                                                  );
+                                                });
+                                          },
+                                          icon: const Icon(
+                                              MdiIcons.emailAlertOutline),
+                                          label: const Text('Verify email')),
+                                    ),
+                                  )
+                                : const SizedBox.shrink(),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 32),
+                              child: RichText(
+                                textAlign: TextAlign.center,
+                                text: TextSpan(
+                                  children: <TextSpan>[
+                                    TextSpan(
+                                        text: 'Developed by ',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .headline6),
+                                    TextSpan(
+                                        text: 'Davoti Solutions',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .headline6!
+                                            .copyWith(fontFamily: 'mustang')),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Padding(
+                                padding:
+                                    const EdgeInsets.only(top: 16, bottom: 8),
+                                child: TouchableOpacity(
+                                  activeOpacity: 0.4,
+                                  onTap: () {
+                                    launch('https://davotisolutions.com/');
+                                  },
+                                  child: Text(
+                                    'www.davotisolutions.com',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .bodyText2!
+                                        .copyWith(
+                                            color: isDarkMode
+                                                ? Colors.lightBlueAccent
+                                                : Colors.blue),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                )),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 16, bottom: 16),
+                              child: SizedBox(
+                                width: 275,
+                                child: OutlinedButton.icon(
+                                    icon: const Icon(Icons.email_outlined),
+                                    onPressed: () {
+                                      final Uri emailLaunchUri = Uri(
+                                        scheme: 'mailto',
+                                        path: 'contact@davotisolutions.com',
+                                        query: encodeQueryParameters(<String,
+                                            String>{
+                                          'subject': 'Regarding todoaholic'
+                                        }),
+                                      );
+
+                                      launch(emailLaunchUri.toString());
+                                    },
+                                    label: const Text('Contact us')),
+                              ),
+                            ),
+                            Padding(
+                                padding:
+                                    const EdgeInsets.only(top: 8, bottom: 16),
+                                child: SizedBox(
+                                  width: 275,
+                                  child: OutlinedButton.icon(
+                                      onPressed: () {
+                                        launch(
+                                            'https://github.com/andordavoti/todoaholic');
+                                      },
+                                      icon: const Icon(MdiIcons.github),
+                                      label:
+                                          const Text('Source-Code on GitHub')),
+                                )),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 8, bottom: 16),
+                              child: SizedBox(
+                                width: 275,
+                                child: OutlinedButton.icon(
+                                    onPressed: () {
+                                      launch(
+                                          'https://www.buymeacoffee.com/andordavoti');
+                                    },
+                                    icon: const Icon(MdiIcons.beerOutline),
+                                    label: const Text('Buy me a beer')),
+                              ),
+                            ),
+                            kIsWeb
+                                ? Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text(
+                                      "Version: ${packageInfo?.version ?? ''}",
+                                      style:
+                                          Theme.of(context).textTheme.bodyText1,
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  )
+                                : const SizedBox.shrink(),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 8, bottom: 16),
+                              child: SizedBox(
+                                width: 275,
+                                child: OutlinedButton.icon(
+                                  style: ButtonStyle(
+                                    foregroundColor:
+                                        MaterialStateColor.resolveWith(
+                                            (states) => Colors.red),
+                                  ),
+                                  onPressed: () {
+                                    HapticFeedback.mediumImpact();
+                                    showDialog(
+                                        context: context,
+                                        builder: (BuildContext context) {
+                                          return const DeleteAccountDialog();
+                                        });
+                                  },
+                                  icon: const Icon(Icons.delete),
+                                  label: const Text('Delete account'),
+                                ),
+                              ),
+                            ),
                           ],
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 32),
-                        child: RichText(
-                          textAlign: TextAlign.center,
-                          text: TextSpan(
-                            children: <TextSpan>[
-                              TextSpan(
-                                  text: 'Developed by ',
-                                  style: Theme.of(context).textTheme.headline6),
-                              TextSpan(
-                                  text: 'Davoti Solutions',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .headline6!
-                                      .copyWith(fontFamily: 'mustang')),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Padding(
-                          padding: const EdgeInsets.only(top: 16, bottom: 8),
-                          child: TouchableOpacity(
-                            activeOpacity: 0.4,
-                            onTap: () {
-                              launch('https://davotisolutions.com/');
-                            },
-                            child: Text(
-                              'www.davotisolutions.com',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyText2!
-                                  .copyWith(
-                                      color: isDarkMode
-                                          ? Colors.lightBlueAccent
-                                          : Colors.blue),
-                              textAlign: TextAlign.center,
-                            ),
-                          )),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 16, bottom: 16),
-                        child: SizedBox(
-                          width: 275,
-                          child: OutlinedButton.icon(
-                              icon: const Icon(Icons.email_outlined),
-                              onPressed: () {
-                                final Uri emailLaunchUri = Uri(
-                                  scheme: 'mailto',
-                                  path: 'contact@davotisolutions.com',
-                                  query: encodeQueryParameters(<String, String>{
-                                    'subject': 'Regarding todoaholic'
-                                  }),
-                                );
-
-                                launch(emailLaunchUri.toString());
-                              },
-                              label: const Text('Contact us')),
-                        ),
-                      ),
-                      Padding(
-                          padding: const EdgeInsets.only(top: 8, bottom: 16),
-                          child: SizedBox(
-                            width: 275,
-                            child: OutlinedButton.icon(
-                                onPressed: () {
-                                  launch(
-                                      'https://github.com/andordavoti/todoaholic');
-                                },
-                                icon: const Icon(MdiIcons.github),
-                                label: const Text('Source-Code on GitHub')),
-                          )),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          "Version: ${packageInfo?.version ?? ''}",
-                          style: Theme.of(context).textTheme.bodyText1,
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.only(top: 16, bottom: 16),
-                        child:
-                            SizedBox(width: 275, child: DeleteAccountButton()),
-                      ),
-                    ],
-                  ),
-                ),
-              ])),
+                    ]);
+                  })),
         ),
       ),
     );
   }
 }
+
+
+// LoadingButton(
+//       isLoading: _isLoading,
+//       color: isCupertino ? CupertinoColors.destructiveRed : Colors.red,
+//       icon: isCupertino ? CupertinoIcons.delete : Icons.delete,
+//       label: l.deleteAccount,
+//       onTap: _deleteAccount,
+//     );
