694302a72eac5d43187683712532e764cf8e9b15
Andor Davoti <andor.davoti@gmail.com>
2022-02-19 17:32:18 +0100

    extend date time extension with toDayStart and toDayEnd

--- client/lib/utils/datetime_extension.dart
+++ client/lib/utils/datetime_extension.dart
@@ -2,4 +2,12 @@ extension MyDateExtension on DateTime {
   DateTime getDateOnly() {
     return DateTime(year, month, day);
   }
+
+  DateTime toDayStart() {
+    return DateTime(year, month, day, 0, 0, 0);
+  }
+
+  DateTime toDayEnd() {
+    return DateTime(year, month, day, 23, 59, 59);
+  }
 }
